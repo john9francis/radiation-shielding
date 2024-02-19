4fe4541125f74c851d95c654b2e09c2765a67299
John Francis <john9francis@gmail.com>
2024-02-19 12:04:33 -0700

    Added some more guidance to the pga messenger

--- src/PGAMessenger.cc
+++ src/PGAMessenger.cc
@@ -1,5 +1,6 @@
 #include "PGAMessenger.hh"
 
+#include "G4Exception.hh"
 
 namespace rad_shield {
 	PGAMessenger::PGAMessenger(PrimaryGeneratorAction* PGA) {
@@ -9,7 +10,10 @@ namespace rad_shield {
 		fDirectory->SetGuidance("Commands to give the primary particles' energies some distribution instead of just one value");
 
 		fSetRightSkewCmd = new G4UIcmdWithADoubleAndUnit("/pga/setRightSkew", this);
-		fSetRightSkewCmd->SetGuidance("Creates a right skewed distribution, with you specifying the mean.");
+		fSetRightSkewCmd->SetDefaultUnit("MeV");
+		fSetRightSkewCmd->SetDefaultValue(.511);
+		fSetRightSkewCmd->SetParameterName("sigma", true);
+		fSetRightSkewCmd->SetGuidance("Creates a right skewed energy distribution. Mean is the gun's energy. You must specify the standard deviation.");
 		fSetRightSkewCmd->AvailableForStates(G4State_PreInit, G4State_Idle);
 
 		fResetCmd = new G4UIcmdWithoutParameter("/pga/reset", this);
@@ -28,7 +32,8 @@ namespace rad_shield {
 
 	void PGAMessenger::SetNewValue(G4UIcommand* command, G4String newValue) {
 		if (command == fSetRightSkewCmd) {
-			G4cout << "Setting right skew with mean of: " << newValue << G4endl;
+			G4cout << "Setting right skew with standard deviation of: " << newValue << G4endl;
+
 		}
 		if (command == fResetCmd) {
 			G4cout << "Resetting..." << G4endl;
